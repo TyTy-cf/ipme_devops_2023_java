b88db4a7d5f1c72c75759dc7f42ce4e24dd20e65
Kevin Tourret <kevin@drosalys.fr>
2023-04-03 16:37:00 +0200

    update exercises until 5

--- src/main/java/fr/ipme/Main.java
+++ src/main/java/fr/ipme/Main.java
@@ -4,11 +4,24 @@ import fr.ipme.entity.*;
 import fr.ipme.exception.PawsException;
 import fr.ipme.entity.interfaces.IShout;
 
+import java.util.Scanner;
+
+
 public class Main {
 
     private long id = 0L;
 
-    public static void main(String[] args) throws PawsException {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+//        cours();
+//        exo1();
+//        exo2();
+//        exo3();
+        exo4();
+    }
+
+    private static void cours() {
         Dog dog = new Dog();
         Animal anOtherDog = new Dog();
         Cat cat = new Cat();
@@ -37,7 +50,100 @@ public class Main {
         } finally {
             System.out.println("Je suis du code exécuté tout le temps !");
         }
+    }
 
+    private static void exo1() {
+        boolean success = false;
+        Double first = null;
+        Double second = null;
+        while (!success) {
+            try {
+                if (first == null) {
+                    System.out.println("Saisir un premier nombre :");
+                    first = scanner.nextDouble();
+                }
+                if (second == null) {
+                    System.out.println("Saisir un deuxième nombre :");
+                    second = scanner.nextDouble();
+                }
+                if (first != null && second != null) {
+                    success = true;
+                    System.out.println("Le premier nombre est : " + first);
+                    System.out.println("Le deuxième nombre est : " + second);
+                    System.out.println("La somme est : " + (first + second));
+                }
+            } catch (Exception e) {
+                scanner.next();
+            }
+        }
+    }
+
+    private static void exo2() {
+        boolean success = false;
+        Integer evenOdd = null;
+        while (!success) {
+            try {
+                if (evenOdd == null) {
+                    System.out.println("Saisir un nombre entier :");
+                    evenOdd = scanner.nextInt();
+                }
+                if (evenOdd != null) {
+                    success = true;
+                    System.out.println("Le nombre est : " + evenOdd);
+                    if (evenOdd % 2 == 0) {
+                        System.out.println("Le nombre est pair !");
+                    } else {
+                        System.out.println("Le nombre est impair !");
+                    }
+                }
+            } catch (Exception e) {
+                scanner.next();
+            }
+        }
+    }
+
+    private static void exo3() {
+        System.out.println("Saisir une chaine de caractères :");
+        String str = scanner.nextLine();
+        System.out.println("La chaine de caractères est : " + str);
+        System.out.println("Sa taille est de : " + str.length());
+    }
+
+    private static void exo4() {
+        // Écrire un programme qui demande des entiers,
+        // les met dans un tableau et qui affiche la somme des éléments du tableau
+        // (il y aura peut-être une étape préalable…)
+        boolean success = false;
+        Integer intNumber = null;
+        while (!success) {
+            try {
+                if (intNumber == null) {
+                    System.out.println("Combien d'entier voulez vous insérer dans le tableau ?");
+                    intNumber = scanner.nextInt();
+                }
+                if (intNumber != null) {
+                    success = true;
+                    int[] array = new int[intNumber];
+                    int sum = 0;
+                    for (int i = 0; i < intNumber; i++) {
+                        System.out.println("Saisir le chiffre n°" + (i+1));
+                        int aNumber = scanner.nextInt();
+                        array[i] = aNumber;
+                        sum += aNumber;
+                    }
+                    System.out.println("Vous avez saisi les chiffres suivant : ");
+                    StringBuilder sb = new StringBuilder();
+                    for (int aNumber: array) {
+                        sb.append(aNumber);
+                        sb.append(" ");
+                    }
+                    System.out.println(sb);
+                    System.out.println("La somme totale est : " + sum);
+                }
+            } catch (Exception e) {
+                scanner.next();
+            }
+        }
     }
 
     @Override
